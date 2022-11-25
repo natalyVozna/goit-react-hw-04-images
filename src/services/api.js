264954aa4e829c3ebb795339bf9f1b734d5468e2
natalyVozna <109243941+natalyVozna@users.noreply.github.com>
2022-11-25 09:26:04 +0200

    update

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,4 @@
 import axios from 'axios';
-// import img from '../assets/autumn-poolside.jpeg';
-// import img1 from '../assets/img1.jpg';
-// import img2 from '../assets/img2.jpg';
 
 const key = '30593721-3615c14b1fd526cc46c7cd9ff';
 const photosApi = axios.create({
@@ -13,66 +10,6 @@ const photosApi = axios.create({
     page: 1,
   },
 });
-
-// const data = {
-//   hits: [
-//     {
-//       webformatURL: `${img}`,
-//       largeImageURL: `${img}`,
-//       tags: 'dfjdsbfa',
-//       id: 1,
-//     },
-//     {
-//       webformatURL: `${img1}`,
-//       largeImageURL: `${img1}`,
-//       tags: 'dfjdsbfa',
-//       id: 2,
-//     },
-//     {
-//       webformatURL: `${img2}`,
-//       largeImageURL: `${img2}`,
-//       tags: 'dfjdsbfa',
-//       id: 3,
-//     },
-//     {
-//       webformatURL: `${img}`,
-//       largeImageURL: `${img}`,
-//       tags: 'dfjdsbfa',
-//       id: 4,
-//     },
-//     {
-//       webformatURL: `${img1}`,
-//       largeImageURL: `${img1}`,
-//       tags: 'dfjdsbfa',
-//       id: 5,
-//     },
-//     {
-//       webformatURL: `${img2}`,
-//       largeImageURL: `${img2}`,
-//       tags: 'dfjdsbfa',
-//       id: 6,
-//     },
-//     {
-//       webformatURL: `${img}`,
-//       largeImageURL: `${img}`,
-//       tags: 'dfjdsbfa',
-//       id: 7,
-//     },
-//     {
-//       webformatURL: `${img1}`,
-//       largeImageURL: `${img1}`,
-//       tags: 'dfjdsbfa',
-//       id: 8,
-//     },
-//     {
-//       webformatURL: `${img2}`,
-//       largeImageURL: `${img2}`,
-//       tags: 'dfjdsbfa',
-//       id: 9,
-//     },
-//   ],
-//   total: 27,
-// };
 
 export const getPtotos = async params => {
   const response = await photosApi.get(`/?key=${key}`, { params });
